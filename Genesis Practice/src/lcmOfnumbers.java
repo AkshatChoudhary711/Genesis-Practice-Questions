3f98054e2ed943b6a1ae6ef2d95adca6cf30037a
Akshat Choudhary <choudharyakshat711@gmail.com>
2025-02-18 11:07:47 +0530

    lcm of numbers

--- Genesis Practice/src/lcmOfnumbers.java
+++ Genesis Practice/src/lcmOfnumbers.java
@@ -1,3 +1,15 @@
 public class lcmOfnumbers {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number A: ");
+		int a = sc.nextInt();
+		System.out.print("Enter number B: ");
+		int b = sc.nextInt();
+		sc.close();
+
+		int lcm = (a*b)/HcfOfNumbers.gcd(a,b);
+		System.out.println("LCM: "+lcm);
+	}
+
 
 }
